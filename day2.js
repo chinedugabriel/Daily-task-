67865ff4ef2c9859ac121f9b3b613990b17c0f6d
chinedugabriel <chinedugabriel29@gmail.com>
2021-12-14 09:06:58 +0100

    Coverd variable's data Types, Hoisting and Scope

--- day2.js
+++ day2.js
@@ -1,3 +1,10 @@
-let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
+// let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
 // fruits.toString()
-console.log(fruits[2].split("", 3));
+// console.log(fruits[2].split("", 3));
+
+let name1;
+name1 = "Freelance with the best Silicon Valley companies in a number of software jobs";
+let ExWord = name1.slice(15, 48);
+console.log( ExWord );
+console.log( name1);
+ 
