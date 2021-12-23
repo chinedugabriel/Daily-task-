f1766ae9d47ca3dc8751b9cd11eace6d21820232
chinedugabriel <chinedugabriel29@gmail.com>
2021-12-23 05:17:07 +0100

    learnt Slice,substring, substr

--- day2.js
+++ day2.js
@@ -2,9 +2,13 @@
 // fruits.toString()
 // console.log(fruits[2].split("", 3));
 
+
+// Extracting Sting parts
+// slice[ take two values[start, end] and also takes negative values], substring[takes two positive values only], substr[takes two values, the specified location and the specified of words ]
+
 let name1;
 name1 = "Freelance with the best Silicon Valley companies in a number of software jobs";
-let ExWord = name1.slice(15, 48);
-console.log( ExWord );
-console.log( name1);
+let result = name1.substr(15, 48);
+console.log( result);
+// console.log( name1);
  
