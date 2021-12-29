f73b9b10a483db6ae1e908fd241b345da77ad903
chinedugabriel <chinedugabriel29@gmail.com>
2021-12-29 06:34:47 +0100

    learnt about Replace()

--- day2.js
+++ day2.js
@@ -1,14 +1,16 @@
-// let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
+let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
 // fruits.toString()
 // console.log(fruits[2].split("", 3));
 
 
-// Extracting Sting parts
-// slice[ take two values[start, end] and also takes negative values], substring[takes two positive values only], substr[takes two values, the specified location and the specified of words ]
+// 
 
-let name1;
-name1 = "Freelance with the best Silicon Valley companies in a number of software jobs";
-let result = name1.substr(15, 48);
-console.log( result);
-// console.log( name1);
+let name1 = "Freelance with the best of the best Silicon Valley companies in a number of software jobs";
+// let result = name1.replace("Best", "new");
+
+// the replace method is case sensitive to avoid this we use a regular expression /word/i to make it insensitive
+// let result = name1.replace(/Best/i, "new");
+
+let result = name1.replace(/best/g, "new");
+console.log(result);
  
