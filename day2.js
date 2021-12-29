cd8971426a2a38242b6cc8d2db7ef12d258f4fce
chinedugabriel <chinedugabriel29@gmail.com>
2021-12-29 16:52:43 +0100

    Learnt charAt(), chCodeAt() and Property access

--- day2.js
+++ day2.js
@@ -6,14 +6,36 @@ let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
 
 
 let word1 = " Freelance with the best of the best Silicon Valley companies in a number of software jobs  ";
-
-// padding is divided into two padStart and padEnd which support padding at the beginning and at the end of a string
-
-// The padStart() method pads the current string with another string (multiple times, if needed) until the resulting string reaches the given length. The padding is applied from the start of the current string. 
-
 let word2 = "i am more than able";
 
-// let result = word2.padStart(34,"0");
-let result = word2.padEnd(22, " mw");
+// Extracting String Characters
+// There are 3 methods for extracting string characters:
+
+// charAt(position)
+// charCodeAt(position)
+// Property access [ ]
+
+// charAt()
+// The charAt() method returns the character at a specified index (position) in a string
+
+// let result = word2.charAt(10)
+// console.log(result);
+
+
+
+
+// charCodeAt()
+// The charCodeAt() method returns the unicode of the character at a specified index in a string:
+
+// The method returns a UTF-16 code (an integer between 0 and 65535).
+
+// let result = word2.charCodeAt(3)
+// console.log(result);
+
+// Property access []
+// in the process makes it easy to access character in a string like an array.
+
+let result = word2[3]
 console.log(result);
+
  
