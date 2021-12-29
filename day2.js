1d7a6ade7efc3dfa20d960991d58378e88cb0e5e
chinedugabriel <chinedugabriel29@gmail.com>
2021-12-29 15:32:12 +0100

    Learnt PadStart() and PadEnd() methods

--- day2.js
+++ day2.js
@@ -5,11 +5,15 @@ let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
 
 
 
-let name1 = " Freelance with the best of the best Silicon Valley companies in a number of software jobs  ";
+let word1 = " Freelance with the best of the best Silicon Valley companies in a number of software jobs  ";
 
-// trim() method
-//its is used to removes whitespace from both sides of a string
-let vocal = "   i am more than able   ";
-let result = vocal.trim();
+// padding is divided into two padStart and padEnd which support padding at the beginning and at the end of a string
+
+// The padStart() method pads the current string with another string (multiple times, if needed) until the resulting string reaches the given length. The padding is applied from the start of the current string. 
+
+let word2 = "i am more than able";
+
+// let result = word2.padStart(34,"0");
+let result = word2.padEnd(22, " mw");
 console.log(result);
  
