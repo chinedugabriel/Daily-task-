4bd7c0d9fa9864d40f4ee5c1f1ee178a91bf9a0e
chinedugabriel <chinedugabriel29@gmail.com>
2021-12-29 14:58:57 +0100

    learnt trim() Method

--- day2.js
+++ day2.js
@@ -3,11 +3,13 @@ let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
 // console.log(fruits[2].split("", 3));
 
 
-// 
 
-let name1 = "Freelance with the best of the best Silicon Valley companies in a number of software jobs";
-// concat() method
-// it used to join two or more words together, its same as using +  operator 
-let result = name1.concat(". ", "\n", "javascript is the language");
+
+let name1 = " Freelance with the best of the best Silicon Valley companies in a number of software jobs  ";
+
+// trim() method
+//its is used to removes whitespace from both sides of a string
+let vocal = "   i am more than able   ";
+let result = vocal.trim();
 console.log(result);
  
