289737128b605950c0f0f571c11547d60ef55314
chinedugabriel <chinedugabriel29@gmail.com>
2022-01-09 22:23:27 +0100

    learnt numbers curently in number methods

--- day2.js
+++ day2.js
@@ -6,27 +6,36 @@ let fruits = ["Banana", "Orange", "Apple", "Mango", "Melon"];
 let word1 = " Freelance with the best of the best Silicon Valley companies in a number of software jobs  ";
 let word2 = "i am more than able";
 
-let Num = "12345678910";
-let Num2 = "1,2,3,4,5,6,7,8,9,10";
+// let Num = "12345678910";
+// let Num2 = "1,2,3,4,5,6,7,8,9,10";
 
 let letters = "abcdefghijklmnopqrstuvwxyz";
 
-// Mactch() method
-//  it retrieves the result of matching a string against a regular expression and returns the matches, as an Array object.
+// Number
+// It can be writen with or with out decimal 
+ 
+// // Exponential way of writing 
+// let numEx2 = 123e-5;
+// // its same as 
+// let numEx = 123 / 100000; 
+// // or 
+// let numEx2 = 123e5;
+// // it same as 
+// let numEx = 123 * 100000; 
 
-// let result = word2.match(/a/gi);
-// console.log(result);
+// Number method 
+//toString() method returns a number as a string
+ 
+let cu = 19.4152;
 
-//  Regular Expression 
-// A regular expression is a sequence of characters that forms a search pattern.
-// Eg:   var part = /wordMan/i;
+// console.log(cu.toString());
+// console.log(cu.toExponential(2));
 
-// examples of methods that uses regular expression are:
-// search(), Replace(), match(), etc...
+// toExponential() 
+// It returns a string, with a number rounded and written using exponential notation.
 
+// toFixed() Method 
+// toFixed() returns a string, with the number written with a specified number of decimals:
+// it's more like rounding up to the given nearest decimal
 
-// using regular expression object 
-// let re = new RegExp('ab+c');
-
-let reg = new RegExp("n", "m");
-console.log(word1.match(reg));
+console.log(cu.toFixed(2));
