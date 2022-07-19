085f3389b409de5d5c30d29dd6f3a360336c6d92
chinedugabriel <chinedugabriel29@gmail.com>
2022-07-19 23:13:52 +0100

    learnt about callBacks, promises and chaining promise

--- Revision.js
+++ Revision.js
@@ -425,4 +425,336 @@ scriptLoaderPromise.then(
 //   checkNumber(NaN);
 //   checkNumber("waawoo");
 
+/*
+let names = function (x){
+  return new Promise((resolve,reject)=>{
+    let inputName = x;
+    if (typeof inputName === 'string'){
+      resolve(`${inputName} is a string`);
+    }else{
+      reject(new Error("text only"));
+    }
+  });
+}
+
+let checkNames = (x) => names(x)
+.then(
+  value => console.log(value),
+  error => console.log(error)
+  )
+  .finally(
+    setTimeout(() => {
+      console.log("Thanks for coming");
+    },1000)
+  );
+
+  checkNames("423");
+
+  // If we’re interested only in errors
+  
+  let staffNames = new Promise((resolve,reject) => {
+    // resolve();
+    reject("Error");
+  })
+// .then(
+//   null,
+//   (error)=> console.log(error)
+//   );
+  
+  .catch(
+  (error)=> console.log(error)
+  );
+
+*/
+
+  // function showConsole(x){//this function is used as a callback
+  //   console.log(x);
+  // }
+
+  // let test = new Promise(resolve =>{
+  //   resolve("we just resolved our promise");
+  // })
+
+  // test.then(value => console.log(value + " next task"));
+  // test.then(showConsole);
+
+  // // A promise that displays it's resolve in 3 seconds
+  // function delay(ms){
+  //   return new Promise((resolve) =>{
+  //     setTimeout(resolve("coding is life"),ms)
+  //   });
+  // }
+
+  // delay(4000).then(
+  //   (value) => console.log(value)
+  // )
+
+// --------------------------------------------------
+  
+// Promise chaining 
+  
+  // let demo = new Promise((resolve, reject)=>{
+  //   let  pass = true;
+  //   pass ? resolve(1) : reject("Try again");
+  // }).then(
+  //   (result)=>{
+  //     console.log(result);
+  //     return result * 2;
+  //   },
+  //   (error)=>console.log(error)
+  // ).then(
+  //   (result)=>{
+  //     console.log(result);
+  //     return result * 2;
+  //   },
+  //   (error)=>console.log(error)
+  // ).then(
+  //   (result)=>{
+  //     console.log(result);
+  //     return result * 2;
+  //   },
+  //   (error)=>console.log(error)
+  // );
+
+//   Returning promises
+// A handler, used in .then(handler) may create and return a promise.
+
+// In that case further handlers wait until it settles, and then get its result.
+
+// For instance:
+
+// let demo2 = new Promise(
+//   (resolve, reject)=>{
+//     let test = true;
+//     test ? resolve(1) : reject("Try again");
+//   }).then(
+//     (value)=>{
+//       console.log(value);
+//       return new Promise((resolve, reject) => {
+//         setTimeout(()=> resolve(value * 2), 2000)
+//       });
+//     }
+//   ).then(
+//     (value)=>{
+//       console.log(value);
+//       return new Promise(
+//         (resolve, reject)=>{
+//           setTimeout(()=> resolve( value * 2), 3000)
+//         });
+//     }
+//   ).then(
+//     (value)=>{
+//       console.log(value);
+//       return new Promise(
+//         (resolve, reject)=>{
+//           setTimeout(()=> resolve( value * 2), 3000)
+//         });
+//     }
+//   ).then(
+//     (value)=>{
+//       console.log(value)
+//       setTimeout(()=> console.log( value * 2), 3000)
+//     }
+//   )
+
+
+  // Thenables
+  // In JavaScript, a thenable is an object that has a then() function. All promises are thenables, but not all thenables are promises.
+
+// class Thenable {
+//   constructor(_num){
+//     this.num = _num;
+//   }
+//   then(resolve, reject){
+//     // console.log(resolve);
+//     setTimeout(()=> resolve(this.num * 2), 2000);
+//   }
+
+// }
+
+// new Promise(resolve => resolve(1))
+//   .then(result => {
+//     return new Thenable(result);
+//   })
+//   .then(showConsole);
+
+
+//===================
+// let customer = new Promise((resolve, reject)=>{
+//   let customerName = true;
+//   customerName == true ? resolve(1) : reject("not found");
+// }).then(
+//   (result)=>{
+//     console.log(result);
+//     return new Promise((resolve)=>{
+//       setTimeout(()=>resolve(result * 2),2000);
+//     })
+//   },
+//   (error)=>console.log(error)
+// ).then(
+//   (result)=>{
+//     console.log(result);
+//     return new Promise((resolve)=>{
+//       setTimeout(()=>resolve(result * 2),2000);
+//     })
+//   },
+//   (error)=>console.log(error)
+// ).then(
+//   (result)=>{
+//     console.log(result);
+//   },
+//   (error)=>console.log(error)
+// );
+
+// class Thenable{
+//   constructor(_num){
+//     this.num = _num;
+//   }
+//   then(resolve, reject){
+//     setTimeout(()=>resolve(this.num * 2),2000)
+//   }
+// }
+
+// new Promise( resolve => resolve(1) )
+// .then( result =>{
+//   //  console.log(result)
+//     return new Thenable(result)
+//   }).then(
+//     result => console.log(result)
+//   )
+
+
+// class able{
+//   constructor(num){
+//     this.num = num;
+//   }
+//   then(resolve,reject){
+//     resolve(this.num * 2)
+//   }
+// }
+
+// new Promise( resolve => resolve(3))
+// .then( result =>{
+//   return new able(result)
+// }).then(
+//   result=>console.log(result)
+// )
+
+
+// let vibe = (x)=> new Promise(resolve=>{
+//   resolve(x)
+// }).then(
+//   (result)=> new able(result)
+// ).then(
+//   result=>console.log(result)
+// )
+
+// vibe(12);
+// vibe(6);
+
+
+// Async and Await
+
+let showAnsConsole = (x) => console.log(x); 
+
+// // async function myJade(){
+// //   return "I am Jade";
+// // }
+
+// async function myJade(){
+//   return Promise.resolve("I am Jade");
+// }
+
+// myJade().then(
+//   result => showAnsConsole(result),
+//   error => showAnsConsole(error)
+// );
+
+
+let goal = new Promise((resolve,reject)=>{
+  return resolve(1);
+})
+
+let leo = async () => goal;
+// async function leo(){
+//   return goal;
+// }
+
+ let leoSmart = () => Promise.resolve(goal); 
+//  function leoSmart(){
+//   return Promise.resolve(goal);
+//  }
+
+ console.log(goal === leo());
+ console.log(goal === leoSmart());
+
+ 
+ let f = async () => 1;
+
+ f().then(
+  showAnsConsole
+ )
+
+//  class bugfix{
+//   constructor(_name){
+//     this.name =_name;
+//   }
+//   then(resolve,reject){
+//     resolve(`${this.name} you have no bugs`)
+//   }
+//  }
+
+
+//  let nameChecker = (x) => new Promise((resolve, reject)=>{
+//   let name = x;
+//   typeof name === "string" ? resolve(`${x} is an actual string`):reject("not a string");
+  
+//  }).then(
+//   result =>{
+//      console.log(result)
+//     return new Promise(resolve => resolve(result))
+//     },
+//   error => console.log(error)
+//  ).then(
+//   result => new bugfix(result)
+//  ).then(
+//   result => console.log(result)
+//  )
+
+
+//  nameChecker("Nedu");
+
+// AWAIT
+// await literally suspends the function execution until the promise settles, and then resumes it with the promise result.
+
+let show = (x)=> console.log(x);
+
+// let vibe = async ()=> "I promise to know this";
+// vibe().then(
+//   result => console.log(result)
+// )
+
+// let vibe = async () => await new Promise(resolve => resolve("I promise to know this"));
+// vibe().then(
+//   result => console.log(result)
+// )
+
+let boostVibe = async () => await Promise.resolve("Yess we Won");
+
+boostVibe().then(show);
+
+// test await in a regular function to see the kind of error it produces
+
+// let er = ()=>{await console.log("wow")};
+
+// er(); //SyntaxError: await is only valid in async functions and the top level bodies of modules  
+
+
+
+
+// Fetch() : it helps us to send network request and get information from the server.
+
+// let request = fetch();
+// the fetch API provides javascript interface for accessing and manipulating parts of the HTTP pipeline, such as requests and responses. 
+
 
